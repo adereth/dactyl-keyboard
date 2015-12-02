ac0ff9c4862ed6249cd160decee2d2e219e79b80
Matt Adereth <adereth@gmail.com>
2015-12-01 21:19:52 -0800

    New model!

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject dactyl-cave "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject dactyl-keyboard "0.1.0-SNAPSHOT"
+  :description "A parametrized, split-hand, concave, columnar, erogonomic keyboard"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [unicode-math "0.2.0"]
-                 [scad-clj "0.1.0"]])
+                 [scad-clj "0.4.0"]])
+
+
--- src/dactyl_cave/core.clj
+++
@@ -1,6 +0,0 @@
-(ns dactyl-cave.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
