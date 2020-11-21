32559bd224f03b0206f6e9579e1e130041a46045
David H. Bronke <whitelynx@gmail.com>
2020-11-21 12:03:45 -0500

    Upgrade scad-clj

--- project.clj
+++ project.clj
@@ -5,6 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [unicode-math "0.2.0"]
-                 [scad-clj "0.4.0"]])
-
-
+                 [scad-clj "0.5.3"]])
